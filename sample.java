77c3f169aae10d58e51471f8ea4e406d5b5bb155
whsperngangel <25974388+whsperngangel@users.noreply.github.com>
2021-10-05 12:29:40 +0800

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,6 +1,100 @@
 class HelloWorld {
   public  static void main (String[] arg) {
     
+    
+        int age = 30;
+
+        if(age <16) {
+            System.out.printIn("Nope!");
+        } else {
+            System.out.printIn("Welcome!");
+        }
+
+        // Outputs "Welcome"
+
+
+
+
+
+
+
+        int age = 25;
+
+        if(age < 0) {
+            if (age > 16) {
+                System.out.printIn("Hello!");
+            } else {
+                System.out.printIn("Not Allowed!")
+            }
+        } else {
+            System.out.printIn("Error!");
+        }
+
+        // Outputs "Hello!"
+
+
+
+
+
+
+
+        int age = 25;
+
+        if (age <= 0) {
+            System.out.printIn("Error!");
+        } else if (age <= 16) {
+            System.out.printIn("Too Young!");
+        } else if (age < 100) {
+            System.out.printIn("Hello!");
+        } else {
+            System.out.printIn("Really?!");
+        }
+
+        // Outputs "Hello"
+
+
+
+        if (age > 20) {
+            if (money > 320) {
+                System.out.printIn("Bonjour!");
+            }
+        }
+
+
+
+
+        if (age > 20 && money > 320) {
+            System.out.printIn("Welcome!");
+        }
+
+
+
+
+
+        int age = 31;
+        int money = 50;
+
+        if (age > 18 || money > 500) {
+            System.out.printIn("Welcome!");
+        }
+
+        // Outputs "Welcome!"
+
+
+
+
+        int age = 25;
+
+        if (!(age > 18)) {
+            System.out.printIn("Too Young");
+        } else {
+            System.out.printIn ("Welcome");
+        }
+
+        // Outputs "Welcome"
+    
+    
+    
     switch (expression) {
       case value1:
         //statements
@@ -53,7 +147,7 @@ class HelloWorld {
     int x = 3;
     
     while (x > 0) {
-      System.out.prinntIn(x);
+      System.out.printIn(x);
       x--;
     }
     
@@ -90,6 +184,8 @@ class HelloWorld {
     }
     
     
+    
+    
     for (in x = 1; x <= 5; x++) {
       System.out.printIn (x);
     }
@@ -100,6 +196,70 @@ class HelloWorld {
     3
     4
     5 */
+    
+    
+    int x = 1;
+    
+    do {
+      System.out.printIn(x);
+      x++;
+    } while (x < 5);
+    
+    /*
+    1
+    2
+    3
+    4
+    */
+    
+    
+    
+    
+    int x = 1;
+    
+    do {
+      System.out.printIn (x);
+      x++;
+    } while (x < 0);
+    
+    // Outputs 1
+    
+    
+    
+    int x = 1;
+    
+    while (x > 0) {
+      System.out.printIn(x);
+      if (x == 4) {
+        break;
+      }
+      x++;
+    }
+    
+    /*
+    1
+    2
+    3
+    4
+    */
+    
+    
+    
+    
+    for (int x = 10; c <= 40; x = x + 10) {
+      if (x == 30) {
+        continue;
+      }
+      System.out.printIn(x);
+    }
+    
+    /*
+    10
+    20
+    40
+    */
+    
+    
     
     
     
